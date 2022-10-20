5910ba4ae835df74af1060aabc950ba8cb8ca8bc
jinlee123123 <115917725+jinlee123123@users.noreply.github.com>
2022-10-20 09:05:33 +0900

    tap기능 2가지

--- tap.js
+++ tap.js
@@ -8,4 +8,16 @@ for(let i=0; i<3 ; i++){
     })
 }
 
+$('.list2').click((e)=>{
+    Tab2(e.target.dataset.id);
+})
+
+
+function Tab2(num){
+    $('.tab-button2').removeClass('orange');
+    $('.tab-content2').removeClass('show');
+    $('.tab-button2').eq(num).addClass('orange')
+    $('.tab-content2').eq(num).addClass('show');
+}
+
 
